86ce038d838d1ff31e361b9bb6795d043d460810
Carlo <carlo@alude.com.br>
2023-11-13 10:09:00 -0300

    New stuff

---
+++ scripts/copy-files.sh
@@ -0,0 +1,8 @@
+cp /usr/bin/sb-internet .
+cp /usr/bin/sb-pacpackages .
+cp /usr/bin/sb-disk .
+cp /usr/bin/sb-iplocate .
+cp /usr/bin/sb-nettraf .
+cp /usr/bin/sb-cpu .
+cp /usr/bin/sb-volume .
+cp /usr/bin/sb-battery .
